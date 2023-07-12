333267bd78113edd9929cdd55a7478b60660750e
Nick Harper <nharper@chromium.org>
2023-07-12 20:24:12 +0000

    Disable another test
    I missed this in PRs #131 and #135.

--- redirects_test.go
+++ redirects_test.go
@@ -225,6 +225,10 @@ func TestHSTSOverHTTP(t *testing.T) {
 }
 
 func TestHTTPNoRedirect(t *testing.T) {
+	// Skip this test because it is failing due to relying on behavior of an
+	// external domain: https://github.com/chromium/hstspreload/issues/112.
+	t.SkipNow()
+
 	skipIfShort(t)
 	t.Parallel()
 
